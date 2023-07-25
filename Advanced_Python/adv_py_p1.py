ec4acea38b2f7f7719fbe9a1f4ba35392a8d255b
Keshawn <skeshawn86@gmail.com>
2023-07-24 22:30:29 -0400

    Committing advance python example code

---
+++ Advanced_Python/adv_py_p1.py
@@ -0,0 +1,22 @@
+my_first_list = [2000, 10000, 50000, 'new_string', 'hi_class', 1000000]
+print(my_first_list)
+
+my_second_list = ['Keshawn', 'Courtney', 'Mark', 'Brandon', 'Stephan', 'Henry', 'Alan', 'Joe']
+print(my_second_list)
+
+print(f'{my_first_list[4]} I would like to introduce you to my firend {my_second_list[2]}')
+
+'''
+Tuples
+'''
+my_friends = ('Keshawn', 'Courtney', 'Mark', 'Brandon', 'Stephan', 'Henry', 'Alan', 'Joe')
+print('\n', my_friends)
+
+'''
+Sets
+'''
+my_first_set = set(my_second_list)
+print('\n', my_first_set)
+
+my_second_set= set(('Stephen', 'James', 20000, 40000))
+print(my_second_set)
